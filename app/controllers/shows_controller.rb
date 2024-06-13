677d4805b6580c02e5bf7a3b2e30f5d74136c364
gandradep <eventsonido@gmail.com>
2024-06-13 12:58:53 +0800

    able to create a show with existing venue

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -8,9 +8,9 @@ class ShowsController < ApplicationController
     @shows = Show.includes(:venue, performances: :artists)
                  .all
                  .as_json(
-                   except: %i[id created_at updated_at venue_id],
+                   except: %i[id created_at updated_at],
                    include: {
-                     venue: { except: %i[id created_at updated_at] },
+                     venue: { except: %i[created_at updated_at] },
                      performances: {
                        include: {
                          artists: { except: %i[id created_at updated_at] }
@@ -29,6 +29,17 @@ class ShowsController < ApplicationController
   # POST /shows
   def create
     @show = Show.new(show_params)
+
+    # Check if new_venue parameters are present
+    if show_params[:new_venue].present?
+      @venue = Venue.new(show_params[:new_venue])
+      if @venue.save
+        @show.venue_id = @venue.id
+      else
+        render json: @venue.errors, status: :unprocessable_entity
+        return
+      end
+    end
 
     if @show.save
       render json: @show, status: :created, location: @show
@@ -59,6 +70,9 @@ class ShowsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def show_params
-      params.require(:show).permit(:title, :date, :time, :description, :picture, :url_flyer, :show_recording_link, :venue_id)
+      params.require(:show).permit(
+        :title, :description, :url_flyer, :show_recording_link, :event_date, :venue_id,
+        new_venue: [:name, :url_location, :city, :country]
+      )
     end
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,8 @@
 class Venue < ApplicationRecord
+  has_many :shows
+
+  validates :name, presence: true
+  validates :url_location, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
 end

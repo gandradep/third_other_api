e11d4955a8b9b100517c3b52a11fee43856a37c1
gandradep <eventsonido@gmail.com>
2024-11-10 20:42:18 +0800

    edited show controller to update shows

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -4,37 +4,37 @@ class ShowsController < ApplicationController
   before_action :set_show, only: %i[ show update destroy ]
 
   # GET /shows
-  def index
-    Rails.logger.info "Type of @shows: #{ @shows.class }"
+    def index
+      Rails.logger.info "Type of @shows: #{ @shows.class }"
 
-    @shows = Show.includes(:venue, performances: :artists).all
+      @shows = Show.includes(:venue, performances: :artists).all
 
-    # Retrieve the time zone parameter from the request or a default
-    time_zone_str = params[:time_zone] || 'UTC'
-    time_zone = ActiveSupport::TimeZone[time_zone_str]
+      # Retrieve the time zone parameter from the request or a default
+      time_zone_str = params[:time_zone] || 'UTC'
+      time_zone = ActiveSupport::TimeZone[time_zone_str]
 
-    # Convert the event_date from UTC to the specified time zone
-    shows_with_converted_dates = @shows.map do |show|
-      show_as_json = show.as_json(
-        except: %i[created_at updated_at],
-        include: {
-          venue: { except: %i[created_at updated_at] },
-          performances: {
-            include: {
-              artists: { except: %i[id created_at updated_at] }
+      # Convert the event_date from UTC to the specified time zone
+      shows_with_converted_dates = @shows.map do |show|
+        show_as_json = show.as_json(
+          except: %i[created_at updated_at],
+          include: {
+            venue: { except: %i[created_at updated_at] },
+            performances: {
+              include: {
+                artists: { except: %i[id created_at updated_at] }
+              }
             }
           }
-        }
-      )
+        )
 
-      # Convert event_date from UTC to the specified time zone
-      event_date_local = show.event_date.in_time_zone(time_zone).strftime('%Y-%m-%dT%H:%M:%S')
+        # Convert event_date from UTC to the specified time zone
+        event_date_local = show.event_date.in_time_zone(time_zone).strftime('%Y-%m-%dT%H:%M:%S')
 
-      show_as_json.merge(event_date: event_date_local)
+        show_as_json.merge(event_date: event_date_local)
+      end
+
+      render json: shows_with_converted_dates
     end
-
-    render json: shows_with_converted_dates
-  end
 
   # GET /shows/1
   def show
@@ -74,7 +74,7 @@ class ShowsController < ApplicationController
   # PATCH/PUT /shows/1
   def update
     if @show.update(show_params)
-      render json: @show
+      render_show(@show) # Ensures the response format is consistent with `create`
     else
       render json: @show.errors, status: :unprocessable_entity
     end
@@ -105,6 +105,24 @@ class ShowsController < ApplicationController
       params.require(:new_venue).permit(
         :name, :url_location, :city, :country
       )
+    end
+
+    def render_show(show)
+      time_zone_str = show.time_zone || 'UTC' # Use the stored time zone or default to UTC
+      time_zone = ActiveSupport::TimeZone[time_zone_str]
+      event_date_local = show.event_date.in_time_zone(time_zone).strftime('%Y-%m-%dT%H:%M:%S')
+
+      render json: show.as_json(
+        except: %i[created_at updated_at],
+        include: {
+          venue: { except: %i[created_at updated_at] },
+          performances: {
+            include: {
+              artists: { except: %i[id created_at updated_at] }
+            }
+          }
+        }
+      ).merge(event_date: event_date_local)
     end
 
 end
